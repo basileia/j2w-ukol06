2e86985873136ea861d6a3abf87624e893f9687a
basileia <jane.novak@centrum.cz>
2022-11-20 20:21:20 +0100

    get list of business cards from db

--- src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -1,5 +1,6 @@
 package cz.czechitas.java2webapps.ukol6.controller;
 
+import cz.czechitas.java2webapps.ukol6.repository.VizitkaRepository;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.WebDataBinder;
@@ -10,6 +11,12 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 public class VizitkaController {
 
+    private final VizitkaRepository vizitkaRepository;
+
+    public VizitkaController(VizitkaRepository osobaRepository) {
+        this.vizitkaRepository = osobaRepository;
+    }
+
     // prázdné stringy se převedou na null
     @InitBinder
     public void nullStringBinding(WebDataBinder binder) {
@@ -18,7 +25,8 @@ public class VizitkaController {
 
     @GetMapping("/")
     public ModelAndView seznam() {
-        return new ModelAndView("seznam");
+        return new ModelAndView("seznam")
+                .addObject("seznam", vizitkaRepository.findAll());
     }
 
 }
