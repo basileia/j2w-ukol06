0c30bdbb7691055e95c3a84e3117728708d867eb
basileia <jane.novak@centrum.cz>
2022-11-20 14:11:10 +0100

    VizitkaController - basic, Vizitka entity

---
+++ src/main/java/cz/czechitas/java2webapps/ukol6/controller/VizitkaController.java
@@ -0,0 +1,24 @@
+package cz.czechitas.java2webapps.ukol6.controller;
+
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class VizitkaController {
+
+    // prázdné stringy se převedou na null
+    @InitBinder
+    public void nullStringBinding(WebDataBinder binder) {
+        binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
+    }
+
+    @GetMapping("/")
+    public ModelAndView seznam() {
+        return new ModelAndView("seznam");
+    }
+
+}
